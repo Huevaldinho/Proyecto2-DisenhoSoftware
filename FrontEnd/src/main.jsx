667ff6292227c7aaffe6cf76af934a9b1598ba16
Huevaldinho <47650141+Huevaldinho@users.noreply.github.com>
2023-04-24 18:16:36 -0600

    Estructura y tailwind al proyecto
    Se agrego una estructura base y se instalo Tailwind en las dependencias del proyecto

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+/*
+  !Instalar dependencias del proyecto con el comando: npm install
+  !Para correr el proyecto tiene que ejecutar el comando en la consola: npm run dev
+
+ ! El proyecto se va a organizar de la siguiente forma:
+      *components: Aqui se crean los componentes de toda la aplicacion. Puede ser separado por pantalla.
+      *assets: archivos como imagenes
+      *contexts: Aqui se crean los contextos que se vayan a necesitar.
+      *pages: Las paginas enteras, estas paginas van a utilizar los componentes creados anteriormente.
+      *routes: Se definen las rutas de la aplicacion.
+      *services: Logica de la aplicacion, como la conexion con la api y el controlador.
+*/
