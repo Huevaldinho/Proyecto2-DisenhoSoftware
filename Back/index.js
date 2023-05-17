87262e003d92ed2f756720f0ac9be586335671c6
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-05-16 22:41:46 -0600

    imagen

--- Back/index.js
+++ Back/index.js
@@ -2,6 +2,8 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import multer from "multer";
+import { v2 as cloudinary } from 'cloudinary'
 import inicioRoutes from "./src/routes/inicio.routes.js";
 import profesoresRoutes from "./src/routes/profesor.routes.js";
 import estudiantesRoutes from "./src/routes/estudiantes.routes.js";
@@ -9,11 +11,31 @@ import comentarioRoutes from "./src/routes/comentarios.routes.js";
 import equipoRoutes from "./src/routes/equipo.routes.js";
 import actividadesRoutes from "./src/routes/actividades.routes.js"
 import {systemDB}  from "./src/database/connection.js";
+
 /*import readXlsxFile from "read-excel-file/node";
 import fs from "fs";*/
 
 const app = express();
 systemDB; //conexión con base de datos
+
+// Configurar Cloudinary
+cloudinary.config({ //esto es privado
+    cloud_name: 'dge4tyjn4',
+    api_key: '918428231237698',
+    api_secret: 'VN0HAyWjh0kvBEsKd81ZY8ekE5M'
+  });
+  
+  // Configuración de Multer para manejar la subida de archivos
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); // Directorio donde se guardarán temporalmente los archivos subidos
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname); // Nombre de archivo único
+    }
+  });
+  
+const upload = multer({ storage });
 
 /*NPM NECESARIOS
 npm i express -D
