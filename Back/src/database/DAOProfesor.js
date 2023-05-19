1762fda6897b02bae34314697aebf009e0904434
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-05-19 16:46:23 -0600

    foto en profesor

--- Back/src/database/DAOProfesor.js
+++ Back/src/database/DAOProfesor.js
@@ -1,16 +1,8 @@
-import { json } from "express";
 import mongoose from "mongoose"; //importación de librerias
-import fs from "fs";
-import { v2 as cloudinary } from 'cloudinary'
-import bodyParser from "body-parser";
-import multer from "multer";
+import {
+    subirFotoNube,
+} from '../database/connectionCloud.js';
 
-cloudinary.config({ //esto es privado
-    cloud_name: 'dge4tyjn4',
-    api_key: '918428231237698',
-    api_secret: 'VN0HAyWjh0kvBEsKd81ZY8ekE5M'
-  });
-  
 
 const profesorSchema = new mongoose.Schema({
     codigo: {type: String, required: true},
@@ -140,35 +132,7 @@ export const agregarProfesor = async (DTOProfesor,fileFoto) => {
     }
 };
 
-// Configuración de Multer para manejar la subida de archivos
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/'); // Directorio donde se guardarán temporalmente los archivos subidos
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname); // Nombre de archivo único
-    }
-  });
-  
-const upload = multer({ storage });
 
-function subirFotoNube(path) {
-    return new Promise((resolve, reject) => {
-      cloudinary.uploader.upload(path, (error, result) => {
-        if (error) {
-          console.error(error);
-          reject("11");
-        }
-  
-        // Eliminar el archivo temporal después de subirlo a Cloudinary
-        fs.unlinkSync(path);
-  
-        // Obtener el enlace público de la imagen subida en Cloudinary
-        const imageUrl = result.secure_url;
-        resolve(imageUrl);
-      });
-    });
-  }
 
 //Función que asigna el código cuando se registra un profesor
 async function asignarCodigo(campusP) {
