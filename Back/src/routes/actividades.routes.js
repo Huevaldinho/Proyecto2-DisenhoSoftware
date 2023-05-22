1a8fae447a9f547d2f8e8f3e648235a31dc1e9a6
Sebastián Bermúdez <sebasberacu@gmail.com>
2023-05-22 13:54:08 -0600

    Actividades act. bien

--- Back/src/routes/actividades.routes.js
+++ Back/src/routes/actividades.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 var router = express.Router();
+import multer from "multer";
 
 import {
     getActividades,
@@ -8,11 +9,21 @@ import {
     deleteActividad
 } from "../controllers/actividad.controller.js";
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // Directorio donde se guardarán temporalmente los archivos subidos
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname); // Nombre de archivo único
+  },
+});
+
+const upload = multer({ storage });
 
 // Método get para recuperar todos los estudiantes y mostrarlos
 router.get('/actividades', getActividades);
-router.post('/actividades', postActividad);
-router.put('/actividades', putActividad);
+router.post('/actividades', upload.single('afiche'), postActividad);
+router.put('/actividades', upload.single('afiche'), putActividad);
 router.delete('/actividades', deleteActividad);
 
 export default router;
